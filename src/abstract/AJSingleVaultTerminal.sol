517d33209e375957e3e7abd25486b1b44e23efd8
xBA5ED <0xBA5ED@protonmail.com>
2022-05-24 03:15:34 +0200

    Inital version of implementations

--- src/abstract/AJSingleVaultTerminal.sol
+++ src/abstract/AJSingleVaultTerminal.sol
@@ -22,22 +22,27 @@ abstract contract AJSingleVaultTerminal is AJPayoutRedemptionTerminal {
         AJAssignReason _reason
     ) internal virtual override {
         Vault storage _vault = projectVault[_projectId];
-        
-        // On fee/pay we always do a local balance deposit
-        if(_reason == AJAssignReason.Pay || _reason == AJAssignReason.FeesPaid){
-            _vault.state.localBalance += _amount;
-            return;
+
+        // We never deposit on 'Pay' or 'FeesPaid' to keep them low gas
+        if (_reason != AJAssignReason.Pay && _reason != AJAssignReason.FeesPaid) {
+
+            int256 _targetLocalBalanceDelta = _targetLocalBalanceDelta(_vault, int256(_amount));
+            // Depositing more (usually) does not increase the gas cost
+            // so we use this opertunity to fill up to the target amount
+            if (_targetLocalBalanceDelta > 0) {
+                // Check if the balance is already above the target
+                uint256 _depositAmount = _amount +
+                    uint256(_targetLocalBalanceDelta);
+
+                // Update the accounting
+                _vault.state.localBalance -= uint256(_targetLocalBalanceDelta);
+                _vault.state.shares += _deposit(_vault, _depositAmount);
+                return;
+            }
         }
 
-        int256 _targetLocalBalanceDelta = _targetLocalBalanceDelta(_vault);
-        if(_targetLocalBalanceDelta > 0){
-            // Check if the balance is already above the target
-            uint256 _depositAmount = _amount + uint256(_targetLocalBalanceDelta);
-
-            // Update the accounting
-            _vault.state.localBalance -= uint256(_targetLocalBalanceDelta);
-            _vault.state.shares += _deposit(_vault, _depositAmount);
-        }
+        _vault.state.localBalance += _amount;
+        return;
     }
 
     /**
@@ -63,9 +68,9 @@ abstract contract AJSingleVaultTerminal is AJPayoutRedemptionTerminal {
             _reason == AJReserveReason.DistributePayoutsOf
         ) {
             uint256 _withdrawAmount = _amount;
-            // Withdrawing more (usually) does not increaee the gas cost
+            // Withdrawing more (usually) does not increase the gas cost
             // so we use this opertunity to fill up to the target amount
-            int256 _targetLocalBalanceDelta = _targetLocalBalanceDelta(_vault);
+            int256 _targetLocalBalanceDelta = _targetLocalBalanceDelta(_vault, -int256(_amount));
             // we should always have enough shares to do this since we just checked how much we have in the vault
             if (_targetLocalBalanceDelta < 0) {
                 _withdrawAmount += uint256(-_targetLocalBalanceDelta);
@@ -92,21 +97,30 @@ abstract contract AJSingleVaultTerminal is AJPayoutRedemptionTerminal {
         Vault storage _vault = projectVault[_projectId];
 
         // If an vault is set, get the amount of assets that we have deposited
-        if(address(_vault.impl) != address(0)){
-            _assetsInVault = _vault.impl.convertToAssets(_vault.state.shares);
+        if (address(_vault.impl) != address(0)) {
+            _assetsInVault = _vault.impl.previewRedeem(_vault.state.shares);
         }
 
         // TODO: convert to ETH
         _assets = _assetsInVault + _vault.state.localBalance;
     }
 
-    function _targetLocalBalanceDelta(Vault storage _vault)
+    function _targetLocalBalanceDelta(Vault storage _vault, int256 _change)
         internal
         returns (int256 delta)
     {
         // Get the amount of assets in the vault
+        // TODO: Should we use `convertToAssets` or `previewRedeem` here?
         uint256 _vaultAssets = _vault.impl.convertToAssets(_vault.state.shares);
         uint256 _totalAssets = _vault.state.localBalance + _vaultAssets;
+
+        if(_change >= 0){
+            // Add change to the total balance
+            _totalAssets += uint256(_change);
+        }else{
+            // Subtract the abs of change from the total balance
+            _totalAssets -= uint256(-_change);
+        }
 
         // calculate the target local amount
         uint256 _targetLocalBalance = (_totalAssets / 1_000_000) *
@@ -126,6 +140,7 @@ abstract contract AJSingleVaultTerminal is AJPayoutRedemptionTerminal {
         returns (uint256 sharesReceived);
 }
 
+// TODO: move structs
 struct Vault {
     ERC4626 impl;
     VaultConfig config;
